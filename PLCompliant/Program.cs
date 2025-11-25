9186ccaa34b6cf37ab8bd00566887aea1a445bd3
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-11-25 09:50:03 +0100

    fix biig mistake lol

--- PLCompliant/Program.cs
+++ PLCompliant/Program.cs
@@ -9,7 +9,7 @@ namespace PLCompliant
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static async Task Main()
+        static void Main()
         {
 
 
