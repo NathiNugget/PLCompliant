0eb0c72eb0a1e21187825bce74046d5102d47365
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-11-28 13:18:13 +0100

    Create event classes for popups and args

---
+++ PLCompliant/Events/PopupWindowEvent.cs
@@ -0,0 +1,36 @@
+﻿using PLCompliant.Enums;
+using PLCompliant.EventArguments;
+using PLCompliant.Utilities;
+
+namespace PLCompliant.Events
+{
+    public class PopupWindowEvent : UIRaisedEvent
+    {
+        public PopupWindowEvent(PopupWindowArgs argument) : base(argument)
+        {
+        }
+
+        public override void ExecuteEvent(Form context)
+        {
+            var validatedItems = EventUtilities.ValidateContextAndArgs<Form1, PopupWindowArgs, Form, RaisedEventArgs>(context, Argument);
+            var args = validatedItems.Item2;
+            var form = validatedItems.Item1;
+            switch(args.Type)
+            {
+                case PopupWindowType.ErrorWindow:
+                    MessageBox.Show(args.Message, "Fejl", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                case PopupWindowType.WarningWindow:
+                    MessageBox.Show(args.Message, "Advarsel", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    break;
+                case PopupWindowType.InformationWindow:
+                    MessageBox.Show(args.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                default:
+                    MessageBox.Show(args.Message, "Ukendt Popup Type", MessageBoxButtons.OK, MessageBoxIcon.None);
+                    break;
+
+            }
+        }
+    }
+}
