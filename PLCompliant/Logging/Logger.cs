6af4ca67b5afc85f6af361e2c4efe29c0c545e40
Marius Egede Knudsen <mak004@edu.zealand.dk>
2025-11-28 12:46:10 +0100

    implement the basic logging class and interface

---
+++ PLCompliant/Logging/Logger.cs
@@ -0,0 +1,33 @@
+﻿using PLCompliant.Interface;
+using System.Diagnostics;
+
+namespace PLCompliant.Logging
+{
+    public class Logger : ILogger
+    {
+        private static Logger _instance = new Logger();
+
+
+
+        public static Logger Instance { get { return _instance; } }
+        private TraceSource _source = new("PLCompliant");
+
+        public void SetLogLevel(SourceLevels level)
+        {
+            _source.Switch.Level = level;
+        }
+        public void AddListener(TraceListener listener)
+        {
+            _source.Listeners.Add(listener);
+        }
+        public void RemoveListener(TraceListener listener)
+        {
+            _source.Listeners.Remove(listener);
+        }
+        public void LogMessage(string message, TraceEventType type, int id)
+        {
+            _source.TraceEvent(type, id, message);
+        }
+
+    }
+}
