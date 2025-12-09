fef4113f0e74882bd03c55422442154905f3650a
= <nathaniel.risum2@gmail.com>
2025-12-09 11:54:39 +0100

    Add spacebar to be checked when traversing octets while insertings IPs

--- PLCompliant/PLCompliantUI.Designer.cs
+++ PLCompliant/PLCompliantUI.Designer.cs
@@ -51,10 +51,9 @@
             // StartStopButton
             // 
             StartStopButton.Font = new Font("Segoe UI", 12F);
-            StartStopButton.Location = new Point(132, 624);
-            StartStopButton.Margin = new Padding(4);
+            StartStopButton.Location = new Point(106, 499);
             StartStopButton.Name = "StartStopButton";
-            StartStopButton.Size = new Size(94, 52);
+            StartStopButton.Size = new Size(75, 42);
             StartStopButton.TabIndex = 4;
             StartStopButton.Text = "Start";
             StartStopButton.UseVisualStyleBackColor = true;
@@ -65,21 +64,20 @@
             CurrentStateLabel.AccessibleDescription = "";
             CurrentStateLabel.AccessibleName = "";
             CurrentStateLabel.AutoSize = true;
-            CurrentStateLabel.Location = new Point(748, 656);
-            CurrentStateLabel.Margin = new Padding(4, 0, 4, 0);
-            CurrentStateLabel.MaximumSize = new Size(375, 500);
+            CurrentStateLabel.Location = new Point(598, 525);
+            CurrentStateLabel.MaximumSize = new Size(300, 400);
             CurrentStateLabel.Name = "CurrentStateLabel";
-            CurrentStateLabel.Size = new Size(115, 20);
+            CurrentStateLabel.Size = new Size(133, 15);
             CurrentStateLabel.TabIndex = 1;
             CurrentStateLabel.Text = "After brugerens instruks";
             CurrentStateLabel.TextChanged += CurrentStateLabel_TextChanged;
             // 
             // BrowseButton
             // 
-            BrowseButton.Location = new Point(769, 526);
-            BrowseButton.Margin = new Padding(4, 4, 4, 0);
+            BrowseButton.Location = new Point(615, 421);
+            BrowseButton.Margin = new Padding(3, 3, 3, 0);
             BrowseButton.Name = "BrowseButton";
-            BrowseButton.Size = new Size(94, 29);
+            BrowseButton.Size = new Size(75, 23);
             BrowseButton.TabIndex = 3;
             BrowseButton.Text = "Browse";
             BrowseButton.UseVisualStyleBackColor = true;
@@ -88,19 +86,17 @@
             // SavePath
             // 
             SavePath.Font = new Font("Segoe UI", 9F);
-            SavePath.Location = new Point(105, 526);
-            SavePath.Margin = new Padding(4);
+            SavePath.Location = new Point(84, 421);
             SavePath.Name = "SavePath";
-            SavePath.Size = new Size(655, 27);
+            SavePath.Size = new Size(525, 23);
             SavePath.TabIndex = 2;
             // 
             // SaveFileInstruction
             // 
             SaveFileInstruction.AutoSize = true;
-            SaveFileInstruction.Location = new Point(105, 504);
-            SaveFileInstruction.Margin = new Padding(4, 0, 4, 0);
+            SaveFileInstruction.Location = new Point(84, 403);
             SaveFileInstruction.Name = "SaveFileInstruction";
-            SaveFileInstruction.Size = new Size(195, 20);
+            SaveFileInstruction.Size = new Size(156, 15);
             SaveFileInstruction.TabIndex = 4;
             SaveFileInstruction.Text = "Vælg hvor CSV skal gemmes";
             // 
@@ -108,10 +104,9 @@
             // 
             ModbusButton.AutoSize = true;
             ModbusButton.Checked = true;
-            ModbusButton.Location = new Point(182, 191);
-            ModbusButton.Margin = new Padding(4);
+            ModbusButton.Location = new Point(146, 153);
             ModbusButton.Name = "ModbusButton";
-            ModbusButton.Size = new Size(170, 24);
+            ModbusButton.Size = new Size(139, 19);
             ModbusButton.TabIndex = 0;
             ModbusButton.TabStop = true;
             ModbusButton.Text = "Modbus TCP Port 502";
@@ -121,10 +116,9 @@
             // Step7Button
             // 
             Step7Button.AutoSize = true;
-            Step7Button.Location = new Point(182, 300);
-            Step7Button.Margin = new Padding(4);
+            Step7Button.Location = new Point(146, 240);
             Step7Button.Name = "Step7Button";
-            Step7Button.Size = new Size(185, 24);
+            Step7Button.Size = new Size(147, 19);
             Step7Button.TabIndex = 1;
             Step7Button.Text = "Siemens Step7 Port 102";
             Step7Button.UseVisualStyleBackColor = true;
@@ -133,22 +127,20 @@
             // IPInstructionLabel
             // 
             IPInstructionLabel.AutoSize = true;
-            IPInstructionLabel.Location = new Point(769, 139);
-            IPInstructionLabel.Margin = new Padding(4, 0, 4, 0);
+            IPInstructionLabel.Location = new Point(615, 111);
             IPInstructionLabel.Name = "IPInstructionLabel";
-            IPInstructionLabel.Size = new Size(152, 20);
+            IPInstructionLabel.Size = new Size(122, 15);
             IPInstructionLabel.TabIndex = 7;
             IPInstructionLabel.Text = "Indtast IP-rækkevidde";
             // 
             // FromTextBox
             // 
             FromTextBox.Culture = new System.Globalization.CultureInfo("en-US");
-            FromTextBox.Location = new Point(769, 191);
-            FromTextBox.Margin = new Padding(4);
+            FromTextBox.Location = new Point(615, 153);
             FromTextBox.Mask = "099.099.099.099";
             FromTextBox.Name = "FromTextBox";
             FromTextBox.PromptChar = ' ';
-            FromTextBox.Size = new Size(124, 27);
+            FromTextBox.Size = new Size(100, 23);
             FromTextBox.TabIndex = 5;
             // 
             // Tooltip
@@ -160,70 +152,67 @@
             // ToTextBox
             // 
             ToTextBox.Culture = new System.Globalization.CultureInfo("en-US");
-            ToTextBox.Location = new Point(769, 300);
-            ToTextBox.Margin = new Padding(4);
+            ToTextBox.Location = new Point(615, 240);
             ToTextBox.Mask = "099.099.099.099";
             ToTextBox.Name = "ToTextBox";
             ToTextBox.PromptChar = ' ';
-            ToTextBox.Size = new Size(124, 27);
+            ToTextBox.Size = new Size(100, 23);
             ToTextBox.TabIndex = 6;
             // 
             // FromLabel
             // 
             FromLabel.AutoSize = true;
-            FromLabel.Location = new Point(769, 169);
-            FromLabel.Margin = new Padding(4, 0, 4, 0);
+            FromLabel.Location = new Point(615, 135);
             FromLabel.Name = "FromLabel";
-            FromLabel.Size = new Size(29, 20);
+            FromLabel.Size = new Size(23, 15);
             FromLabel.TabIndex = 12;
             FromLabel.Text = "Fra";
             // 
             // ToLabel
             // 
             ToLabel.AutoSize = true;
-            ToLabel.Location = new Point(769, 277);
-            ToLabel.Margin = new Padding(4, 0, 4, 0);
+            ToLabel.Location = new Point(615, 222);
             ToLabel.Name = "ToLabel";
-            ToLabel.Size = new Size(25, 20);
+            ToLabel.Size = new Size(20, 15);
             ToLabel.TabIndex = 13;
             ToLabel.Text = "Til";
             // 
             // ProtocolInstruction
             // 
             ProtocolInstruction.AutoSize = true;
-            ProtocolInstruction.Location = new Point(182, 138);
-            ProtocolInstruction.Margin = new Padding(4, 0, 4, 0);
+            ProtocolInstruction.Location = new Point(146, 110);
             ProtocolInstruction.Name = "ProtocolInstruction";
-            ProtocolInstruction.Size = new Size(103, 20);
+            ProtocolInstruction.Size = new Size(81, 15);
             ProtocolInstruction.TabIndex = 14;
             ProtocolInstruction.Text = "Vælg protokol";
             // 
             // logTextBox
             // 
-            logTextBox.Location = new Point(1172, 169);
+            logTextBox.Location = new Point(938, 135);
+            logTextBox.Margin = new Padding(2, 2, 2, 2);
             logTextBox.Multiline = true;
             logTextBox.Name = "logTextBox";
             logTextBox.ReadOnly = true;
             logTextBox.ScrollBars = ScrollBars.Vertical;
-            logTextBox.Size = new Size(323, 507);
+            logTextBox.Size = new Size(259, 406);
             logTextBox.TabIndex = 15;
             // 
             // logLabel
             // 
             logLabel.AutoSize = true;
             logLabel.Font = new Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            logLabel.Location = new Point(1172, 98);
+            logLabel.Location = new Point(928, 79);
+            logLabel.Margin = new Padding(2, 0, 2, 0);
             logLabel.Name = "logLabel";
-            logLabel.Size = new Size(98, 60);
+            logLabel.Size = new Size(78, 47);
             logLabel.TabIndex = 16;
             logLabel.Text = "Log";
-            
             // 
             // PLCompliantUI
             // 
-            AutoScaleDimensions = new SizeF(120F, 120F);
+            AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(1580, 851);
+            ClientSize = new Size(1264, 681);
             Controls.Add(logLabel);
             Controls.Add(logTextBox);
             Controls.Add(ProtocolInstruction);
@@ -240,7 +229,6 @@
             Controls.Add(CurrentStateLabel);
             Controls.Add(StartStopButton);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4);
             Name = "PLCompliantUI";
             Text = "PLCompliant";
             Load += PLCompliantUI_Load;
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PLCompliant
+
+This is how the application looks while running (do mind the GIF-compression though)
+![swag](PLCompliant/media/output.gif)
