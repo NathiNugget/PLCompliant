3ce92ed1d24000af9d4ca5ad5e0dc124261df597
Marius Egede Knudsen <mak004@edu.zealand.dk>
2026-01-08 09:36:45 +0100

    testcode

--- PLCompliant/Program.cs
+++ PLCompliant/Program.cs
@@ -1,7 +1,10 @@
 using PLCompliant.Config;
 using PLCompliant.Events;
+using PLCompliant.Scanning;
 using PLCompliant.Utilities;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
 
 namespace PLCompliant
 {
@@ -14,6 +17,58 @@ namespace PLCompliant
         [STAThread]
         static void Main()
         {
+            IPAddressRange range = new IPAddressRange(IPAddress.Parse("192.168.123.0"), IPAddress.Parse("192.168.130.0"));
+            List<Socket> sockets = new List<Socket>((int)range.Count);
+            foreach (IPAddress ip in range)
+            {
+                Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Blocking = false;
+                try
+                {
+                    socket.Connect(ip, 102);
+                }
+                catch (SocketException e)
+                {
+                    // 10035 == WSAEWOULDBLOCK
+                    if (e.SocketErrorCode == SocketError.WouldBlock)
+                    {
+                        sockets.Add(socket);
+                    }
+                    else
+                    {
+                        socket.Dispose();
+                    }
+                }
+
+            }
+            List<Socket> aliveSockets = new List<Socket>();
+            DateTime startTime = DateTime.Now;
+            TimeSpan timeout = TimeSpan.FromSeconds(2);
+            DateTime endTime = startTime + timeout;
+            while (DateTime.Now < endTime)
+            {
+                for (int i = 0; i < sockets.Count; i++)
+                {
+                    if (sockets[i] == null)
+                    {
+                        continue;
+                    }
+                    if (sockets[i].Poll(1, SelectMode.SelectWrite))
+                    {
+                        aliveSockets.Add(sockets[i]);
+                        sockets[i].Dispose();
+                        sockets[i] = null;
+                    }
+                }
+            }
+            sockets.ForEach((socket) => { socket.Dispose(); });
+            while (aliveSockets.Count > 0)
+            {
+                foreach (Socket socket in aliveSockets)
+                {
+                    //do nonblocking socket send/recv
+                }
+            }
 
             ConfigLoader.LoadConfigFile("./config.xml");
 
